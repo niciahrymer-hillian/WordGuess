d9f1248af70299101bb53ed1165ec99635c2eac8
niciahrymer-hillian <rymer.niciah@gmail.com>
2026-04-13 20:49:22 -0400

    Phase 2 core game state added

--- python/wordguess.py
+++ python/wordguess.py
@@ -1,17 +1,22 @@
-"""
-WordGuess game implementation.
 
-@author xt0fer
-@version 1.0.0
-@date 5/27/21 11:02 AM
-"""
-
-
-class WordGuess:
-    """WordGuess game class - ready for implementation."""
-    pass
-    def welcome():
-        print("Welcome to WordGuess!")
-        while True:
-            guess = input("Guess a letter: ")
-            print(f"You guessed: {guess}")
+def welcome():
+    print("Welcome to WordGuess!")
+    while True:
+        guess = input("Guess a letter: ")
+        print(f"You guessed: {guess}")
+class Wordguess:
+    def __init__(self, word):
+        self.word = word.upper()
+        self.guessed_letters = set()
+        self.max_attempts = 6
+        self.wrong_guesses = 0
+    def display_word(self):
+        """Show the word with blanks for the unguessed letters"""
+        display = " "
+        for letter in self.word:
+            if letter in self.guessed_letters:
+                display += letter + " "
+            else:
+                display += "_ "
+        return display.strip()
+    
